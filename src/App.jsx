6673a8d84544c99e6055abb454c95374da04f8a3
Ruth Ramya <bhavani.bolloju@gmail.com>
2023-03-18 13:25:22 +0530

    added styles

--- src/App.jsx
+++ src/App.jsx
@@ -5,3 +5,18 @@ function App() {
 }
 
 export default App;
+
+// ## 👋 Hello! I'm Bhavani
+
+// ### 🙋‍♀️ About Me
+
+// 👉 I'm a self taught front end web developer. \
+// 👉 I love learning new skills. \
+// 👉 In free time i enjoy reading books and solving coding challenges.\
+// 👉 I'm currently learning react js. \
+// 👉 Ask me about HTML, CSS, Javascript and React js.
+
+// #### you can react me:
+
+// [LinkedIn](https://www.linkedin.com/in/bhavani-bolloju-9ba34b241)
+// </br>
